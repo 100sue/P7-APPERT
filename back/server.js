83b67efe972a10ad5a8e2c71c9f094b8a5393f03
appert <scoreur@gmail.com>
2022-03-04 23:35:02 +0100

    ajout du front

--- back/server.js
+++ back/server.js
@@ -21,7 +21,7 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-// Recherche les différentes erreurs et les gère de ma nière appropriée puis save dans le serveur
+// Recherche les différentes erreurs et les gère de manière appropriée puis save dans le serveur
 
 const errorHandler = error => {
   if (error.syscall !== 'listen') throw error;
---
+++ front/src/pages/wall/PostForm.vue
@@ -0,0 +1,33 @@
+<script>
+export default{
+    name:"PostForm"
+}
+</script>
+
+
+<template>
+<div class="form-floating mt-4">
+  <textarea 
+  contenteditable
+  class="form-control" 
+  placeholder="Leave a comment here" 
+  id="floatingTextarea">
+  </textarea>
+  <label for="floatingTextarea">Comments</label>
+</div>
+<div class="d-flex">
+<label for="file-input" class="btn btn-secondary mt-1">Add image</label>
+<input id="file-input" type="file"/>
+<button type="button" class="btn btn-primary mt-1 ms-auto">Post</button>
+</div>
+<hr class="dropdown-divider mt-4">
+</template>
+
+<style module>
+input {
+    display: none;
+}
+body {
+    background-color: #5f5f5f1a !important;
+}
+</style>
